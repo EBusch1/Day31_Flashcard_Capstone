09946e161ac151feac54ec314fd41f020411ac13
EBusch1 <elijah.t.busch@gmail.com>
2023-09-18 01:00:21 -0500

    Removing known cards and restarting when the end is reached.

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+import tkinter.messagebox
 from tkinter import *
 import pandas
 import random
+import os
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-words_to_learn = data.to_dict(orient="records")
 current_card = {}
+words_to_learn = {}
+
+try:
+    card_data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    words_to_learn = original_data.to_dict(orient="records")
+else:
+    words_to_learn = card_data.to_dict(orient="records")
 
 
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(words_to_learn)
+    try:
+        current_card = random.choice(words_to_learn)
+    except IndexError:
+        tkinter.messagebox.showerror(title="No more cards", message="You have reached the end of the cards")
+        os.remove("data/words_to_learn.csv")
+        exit()
+
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
@@ -23,6 +37,13 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def known_card():
+    words_to_learn.remove(current_card)
+    data = pandas.DataFrame(words_to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -45,7 +66,7 @@ incorrect_button = Button(image=incorrect_image, highlightthickness=0, command=n
 incorrect_button.grid(row=1, column=0)
 
 correct_image = PhotoImage(file="images/right.png")
-correct_button = Button(image=correct_image, highlightthickness=0, command=next_card)
+correct_button = Button(image=correct_image, highlightthickness=0, command=known_card)
 correct_button.grid(row=1, column=1)
 
 next_card()
